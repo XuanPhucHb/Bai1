b893a7be46d036e322fff690ad4e2b5d4442fc38
XuanPhucHb <xuanphuchb@gmail.com>
2021-01-15 20:44:12 +0700

    first commit

---
+++ SMClient/Models/StudentClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SMClient.Models
+{
+    public class StudentClient
+    {
+        public int Id { get; set; }
+        public string StudentId { get; set; }
+        public string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public int Gender { get; set; }
+        public string Email { get; set; }
+        public string Introduce { get; set; }
+    }
+}
---
+++ SM_Service/App_Code/Mydbcontext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Summary description for Mydbcontext
+/// </summary>
+public class Mydbcontext : DbContext
+{
+    public Mydbcontext() : base("studentdbcontext")
+    {
+        //
+        // TODO: Add constructor logic here
+        //
+    }
+    public DbSet<Student> Students { get; set; }
+}
